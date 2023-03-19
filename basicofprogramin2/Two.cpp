0239503a4bdf98dbf151cc90e7957499f5905635
sudhirbhargav <sudhirbhargav100@gmail.com>
2023-03-19 17:42:21 +0530

    Completed second lecture and solved all the problem which is in it

--- basicofprogramin2/Two.cpp
+++ basicofprogramin2/Two.cpp
@@ -1,12 +1,146 @@
 #include <iostream>
 using namespace std;
-    int add(int a,int b){
-        int sum = a+b;
-        return sum;
-    }
+    // int add(int a,int b){
+        // int sum = a+b;
+        // return sum;
+    // }
+
+    // int maxnum(int a,int b,int c){
+    //     if(a>b&&a>c){
+    //         return a;
+    //     }
+    //     else if (b>a&&b>c){
+    //         return b;
+    //     }
+    //     else{
+    //         return c;
+    //     }
+    // }
+
+    // void printn(int n){
+    //     for(int i=0;i<n;i++){
+    //         cout << i+1 << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // char grade(int n){
+    //     if(n >= 90){
+    //         return 'A';
+    //     }
+    //     else if(n>=80){
+    //         return 'B';
+    //     }
+    //     else if(n>=70){
+    //         return 'C';
+    //     }
+    //     else{
+    //         return 'E';
+    //     }
+    // }
+
+    // int getsum(int n){
+    //         int sum = 0;
+    //     for(int i=2;i<=n;i=i+2){
+    //         sum = sum + i;
+    //         cout << i << endl;
+    //     }
+    //     return sum;
+    // }
+
+    // float radius(int n){
+    //     return 3.14*(n*n);
+    // }
+
+    // void evenodd(int n){
+    //     if(n%2==0){
+    //         cout << "Even";
+    //     }
+    //     else{
+    //         cout << "odd";
+    //     }
+    // }
+
+    // int factorial(int n){
+    //     int ans = 1;
+    //     for(int i=n;i>0;i--){
+    //         ans = ans * i;
+    //     }
+    //     return ans;
+    // }
+
+    // void primeornot(int n){
+    //     int num = 0;
+    //     for(int i=1;i<=n;i++){
+    //         if(n%i==0){
+    //             num++;
+    //         }
+    //     }
+    //     if(num==2){
+    //         cout << "Prime";
+    //     }
+    //     else{
+    //         cout << "Not Prime";
+    //     }
+    // }
+
+    // void allprimenumber(int n){
+    //     for(int i=2;i<=n;i++){
+    //         int divided = 0;
+    //     for(int j=1;j<=i;j++){
+    //         if(i%j==0){
+    //             divided++;
+    //         }
+    //     }
+    //     if(divided==2){
+    //         cout << "Prime Number = " << i << endl;
+    //     }
+    //     }
+    // }
 int main(){
-    int a = 5;
-    int b = 6;
-   int sum = add(a,b);
-   cout << sum;
+//     int a = 5;
+//     int b = 6;
+//    int sum = add(a,b);
+//    cout << sum;
+
+// int a,b,c;
+// cin >> a >> b >> c;
+// int max = maxnum(a,b,c);
+// cout << max;
+
+// int n;
+// cin >> n;
+// printn(n);
+
+// int n;
+// cin >> n;
+// char finalgrade = grade(n);
+// cout << finalgrade;
+
+// int n;
+// cin >> n;
+// int ans = getsum(n);
+// cout << ans;
+
+// int n;
+// cin >> n;
+// float ans = radius(n);
+// cout << ans;
+
+// int n;
+// cin >> n;
+// evenodd(n);
+
+// int n;
+// cin >> n;
+// int ans = factorial(n);
+// cout << ans;
+
+// int n;
+// cin >> n;
+// primeornot(n);
+
+// int n;
+// cin >> n;
+// allprimenumber(n);
 }
